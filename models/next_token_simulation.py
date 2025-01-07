c206f315bb2230b7c91f8a63989f180ed2cf8675
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-07 14:50:56 -0800

    Update next_token_simulation.py

--- models/next_token_simulation.py
+++ models/next_token_simulation.py
@@ -1,1 +1,15 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def simulate_next_token(sequence, model_path="models/foundation_models/gpt2"):
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+
+    inputs = tokenizer.encode(sequence, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=len(inputs[0]) + 1, do_sample=True, top_k=50, top_p=0.95)
+    next_token = tokenizer.decode(outputs[0], skip_special_tokens=True).split()[-1]
+    
+    print(f"Next token predicted: {next_token}")
+
+if __name__ == "__main__":
+    simulate_next_token("User clicked on a product and")
 
