1c261674d37a230f95dbd516b473d0866438a12d
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-07 15:16:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import argparse
+from src.inference import run_simulation
+
+def main():
+    parser = argparse.ArgumentParser(description="Ads Behavior Simulator")
+    parser.add_argument("--config", type=str, default="config.json", help="Path to config file")
+    args = parser.parse_args()
+
+    run_simulation(config_path=args.config)
+
+if __name__ == "__main__":
+    main()
 
